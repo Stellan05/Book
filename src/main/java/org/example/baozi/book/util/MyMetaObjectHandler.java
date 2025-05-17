e9fd2a5e419df1fd44883c89fe871dceaf7092b6
baozi05 <1374231610@qq.com>
2025-05-17 15:45:52 +0800

    lombok中自动填充创建和更新日期的工具类

--- src/main/java/org/example/baozi/book/util/MyMetaObjectHandler.java
+++ src/main/java/org/example/baozi/book/util/MyMetaObjectHandler.java
@@ -3,7 +3,6 @@ package org.example.baozi.book.util;
 import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
 import org.apache.ibatis.reflection.MetaObject;
 import org.springframework.stereotype.Component;
-
 import java.time.LocalDateTime;
 
 /**
